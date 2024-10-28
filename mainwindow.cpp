303866156c34cd457d761a958967fbab1fa62a73
rongjie <shengrongjie666@163.com>
2024-10-28 16:30:45 +0800

    a simple surface

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,17 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    statusLabel.setMaximumWidth(150);
+    statusLabel.setText("length："+QString::number(0)+"    lines："+QString::number(1));
+    ui->statusbar->addPermanentWidget(&statusLabel);
+
+    statusCursorLabel.setMaximumWidth(150);
+    statusCursorLabel.setText("Ln："+QString::number(0)+"    Col："+QString::number(1));
+    ui->statusbar->addPermanentWidget(&statusCursorLabel);
+
+    QLabel *author=new QLabel(ui->statusbar);
+    author->setText("沈荣杰");
+    ui->statusbar->addPermanentWidget(author);
 }
 
 MainWindow::~MainWindow()
